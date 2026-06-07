aaaa7f9073a4ea0af98f29f0d6fdadb83835f3c1
titi2233 <lisantiziana@gmail.com>
2026-06-07 01:03:00 -0300

    fix(m5): fix eslint errors in fs-polyfill

--- src/lib/db/fs-polyfill.ts
+++ src/lib/db/fs-polyfill.ts
@@ -1,14 +1,13 @@
 import fs from 'node:fs';
 
-// @ts-ignore
 if (typeof fs.readdir === 'undefined' || fs.readdir.name === 'unenv') {
-  // @ts-ignore
-  fs.readdir = function (path, options, callback) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (fs as any).readdir = function (path: unknown, options: unknown, callback: unknown) {
     const cb = typeof options === 'function' ? options : callback;
-    if (cb) cb(null, []);
+    if (typeof cb === 'function') cb(null, []);
   };
-  // @ts-ignore
-  fs.readdirSync = function () {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (fs as any).readdirSync = function () {
     return [];
   };
 }
